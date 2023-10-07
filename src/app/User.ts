79f8dd2edd842e449abcba9abf35a219a921b0e0
dzgierski19 <dzgierskigit@gmail.com>
2023-10-07 22:01:07 +0200

    Record type

--- src/app/User.ts
+++ src/app/User.ts
@@ -11,13 +11,13 @@ export enum USERS_TYPE {
 
 export type UserType = (typeof USERS_TYPE)[keyof typeof USERS_TYPE];
 
-export class User {
-  private readonly NUMBER_LEVEL_TO_ROLE_MAPPER: Record<UserType, string> = {
-    [USERS_TYPE.OWNER]: "OWNER",
-    [USERS_TYPE.ADMIN]: "ADMIN",
-    [USERS_TYPE.BASIC]: "BASIC",
-  };
+const NUMBER_LEVEL_TO_ROLE_MAPPER: Record<UserType, string> = {
+  [USERS_TYPE.OWNER]: "OWNER",
+  [USERS_TYPE.ADMIN]: "ADMIN",
+  [USERS_TYPE.BASIC]: "BASIC",
+};
 
+export class User {
   readonly id = randomUUID();
   readonly createdAt: Date;
   readonly role: string;
@@ -25,11 +25,8 @@ export class User {
   deletedAt?: Date;
 
   constructor(roleLevel: USERS_TYPE, clock: Clock) {
-    this.role = this.NUMBER_LEVEL_TO_ROLE_MAPPER[roleLevel];
+    this.role = NUMBER_LEVEL_TO_ROLE_MAPPER[roleLevel];
     this.roleLevel = roleLevel;
     this.createdAt = clock();
   }
 }
-
-const newUser = new User(USERS_TYPE.BASIC, clock);
-console.log(newUser);
